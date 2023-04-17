5245fadab3ff0a1d0f4343f45d10cbd1fcae7df2
Fabrizio Panato <fbzpanatto@hotmail.com>
2023-04-17 10:38:00 -0300

    Front End in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'year', loadComponent: () => import('./components/year/year.component').then(m => m.YearComponent) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/year/year.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-year',
+  templateUrl: './year.component.html',
+  styleUrls: ['./year.component.scss']
+})
+export class YearComponent {
+
+}
