b85b68b740eba749fa40107e543ba04afc50a950
Fabrizio Panato <fbzpanatto@hotmail.com>
2023-04-18 12:56:09 -0300

    Front end in progress

--- src/app/components/year/year.component.ts
+++ src/app/components/year/year.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FetchDataService } from "src/app/shared/services/fetch-data.service";
 import { NavigationService } from "src/app/shared/services/navigation.service";
 import { CommonModule } from "@angular/common";
-import { Subscription, tap } from "rxjs";
+import { Subscription } from "rxjs";
 
 interface Year {
   id: number,
@@ -39,8 +39,7 @@ export class YearComponent implements OnInit, OnDestroy {
     this.navigationService.setActiveComponent({title: YearComponent.title, url: YearComponent.url});
 
     this._subscription = this.fetchData.getAllData<Year>(YearComponent.resource)
-      .pipe(tap((years) => this._years = years))
-      .subscribe()
+      .subscribe((years) => this._years = years)
   }
 
   ngOnDestroy() {
